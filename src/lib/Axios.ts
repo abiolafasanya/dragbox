c6491b223b5e87e0752f9006144a6f5381250734
abiolafasanya <hezekiahfasanya@gmail.com>
2023-09-20 21:14:10 +0100

    commit

--- src/lib/Axios.ts
+++ src/lib/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let API_URI;
-const apiURL = 'http://localhost:3000';
+const apiURL = 'https://dragboxs.vercel.app'
 if (process.env.NODE_ENV === 'production') {
   API_URI = process.env.NEXTAUTH_URL || apiURL;
 } else {
